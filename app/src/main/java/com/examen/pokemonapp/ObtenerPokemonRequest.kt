5a0c34af87b0377a4ab1b929a0e7f569b24afba8
Angel <angloz780@alu.centroafuera.es>
2022-04-06 12:30:22 +0200

    Initial commit

---
+++ app/src/main/java/com/examen/pokemonapp/ObtenerPokemonRequest.kt
@@ -0,0 +1,33 @@
+package com.examen.pokemonapp
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+var gson = Gson()
+
+class ObtenerPokemonRequest {
+
+    companion object {
+        fun get(): ListaPokemon {
+            val listaPokemon = ListaPokemon()
+            val client = OkHttpClient()
+            for (i in 1..9) {
+                val request = Request.Builder()
+                    .url("https://pokeapi.co/api/v2/pokemon/${i}")
+                    .build()
+                val response = client.newCall(request).execute()
+
+                if (response.isSuccessful) {
+                    response.body?.string().let { responseBody ->
+                        val pokemon = gson.fromJson(responseBody, Pokemon::class.java)
+                        listaPokemon.agregar(pokemon)
+                    }
+
+                } else
+                    println("Algo ha ido mal")
+            }
+            return listaPokemon
+        }
+    }
+}
