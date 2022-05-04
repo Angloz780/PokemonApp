a6c7aea4db9334c5e99a4e3d6ff7823b49d4d18e
Angel <angloz780@alu.centroafuera.es>
2022-05-04 11:17:11 +0200

    PokemonApp con tipos

--- app/src/main/java/com/examen/pokemonapp/ObtenerPokemonRequest.kt
+++ app/src/main/java/com/examen/pokemonapp/ObtenerPokemonRequest.kt
@@ -13,7 +13,7 @@ class ObtenerPokemonRequest {
         fun get(): ListaPokemon {
             val listaPokemon = ListaPokemon()
             val client = OkHttpClient()
-            for (i in 1..151) {
+            for (i in 1..251) {
                 val request = Request.Builder()
                     .url("https://pokeapi.co/api/v2/pokemon/${i}")
                     .build()
